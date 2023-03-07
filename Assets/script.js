230bdbf36149bb25a033ad97de37147ae5ffca81
wyatt otto <wyattotto87@gmail.com>
2023-03-06 21:22:18 -0500

    cleaned up some of the files jsut commited

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     // Functions to open and close a modal
-//     function openModal($el) {
-//       $el.classList.add('is-active');
-//     }
-  
-//     function closeModal($el) {
-//       $el.classList.remove('is-active');
-//     }
-  
-//     function closeAllModals() {
-//       (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-//         closeModal($modal);
-//       });
-//     }
-
-
-// })
-
 var button = document.getElementById('button')
 var modal = document.getElementById('Go-Modal')
 
