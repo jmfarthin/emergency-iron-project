3edae7577cd97ee6f64b2934d96a26c08d0dd65a
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-03-07 14:25:53 -0500

    getExercises function can now pull 5 random exercies using API calls

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,24 +2,55 @@
 
 
 var exerciseKey = '2qHB8VKEzSKFjKONqevhLw==6887eTADrOBvLuN8'
-var upperMuscles = ["lats", "biceps", "chest", "triceps", "abdominals"];
-var lowerMuscles = ["calves", "quadriceps", "hamstrings", "quadriceps", "glutes"];
+var upper = ["lats", "biceps", "chest", "triceps", "abdominals"];
+var lower = ["calves", "quadriceps", "hamstrings", "quadriceps", "glutes"];
 var workoutButton = document.querySelector("#generate-workout");
 var generateCard = document.querySelector("#generate-card");
+var exerciseGroup = [];
+var newExercise = {};
+var muscleGroup;
 
 // function to get exercise API data
 async function getExercises(group) {
+    console.log(typeof (group));
     for (muscle of group) {
         var url = new URL("https://api.api-ninjas.com/v1/exercises");
         url.searchParams.set("muscle", muscle);
         var response = await fetch(url, { headers: { "X-API-Key": exerciseKey } });
         var data = await response.json();
+        let randomNum = Math.floor(Math.random() * 10);
+        console.log(randomNum);
         console.log(data);
+        console.log(data[randomNum].name);
     };
 };
 
+// var muscleGroup = upper;
+// console.log(muscleGroup);
+// getExercises(muscleGroup);
 
-getExercises(upperMuscles);
+function generateCardEventHandler(event) {
+    if (event.target.id === "upper") {
+        getExercises(upper);
+    } else {
+        getExercises(lower);
+    }
+};
+
+generateCard.addEventListener("click", generateCardEventHandler);
+
+// var exercises = [
+//     {
+//         name: "tricep dips",
+//         muscle: "triceps",
+//         equipment: "dip_bars",
+//     },
+//     {
+//         name: "dumbbell benchpress",
+//         muscle: "chest",
+//         equipment: "dumbbells"
+//     }
+// ]
 
 // var armMuscle = "triceps"
 
