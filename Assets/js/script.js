6d0627a4e1c72b5309bb904f63096f549dfbc57d
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-03-07 15:08:34 -0500

    completed getExercises function--exercises are now stored to local storage and are replaced by new exercises every time the generate workout button is pressed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,34 +1,40 @@
 
 
-
+// variables to be used with the following functions: getExercises, generateCardEventHandler, generateCard event listener
 var exerciseKey = '2qHB8VKEzSKFjKONqevhLw==6887eTADrOBvLuN8'
 var upper = ["lats", "biceps", "chest", "triceps", "abdominals"];
 var lower = ["calves", "quadriceps", "hamstrings", "quadriceps", "glutes"];
 var workoutButton = document.querySelector("#generate-workout");
 var generateCard = document.querySelector("#generate-card");
-var exerciseGroup = [];
 var newExercise = {};
 var muscleGroup;
 
 // function to get exercise API data
 async function getExercises(group) {
-    console.log(typeof (group));
+    var exerciseGroup = [];
+    // for each muscle in the upper or lower array, an API call with be made, saving a random exercise to the exerciseGroup variable
+    // which is then stored to localStorage to be used by 'blank' function to render the exercise cards to the page
     for (muscle of group) {
         var url = new URL("https://api.api-ninjas.com/v1/exercises");
         url.searchParams.set("muscle", muscle);
         var response = await fetch(url, { headers: { "X-API-Key": exerciseKey } });
         var data = await response.json();
-        let randomNum = Math.floor(Math.random() * 10);
-        console.log(randomNum);
-        console.log(data);
-        console.log(data[randomNum].name);
+        let ranNum = Math.floor(Math.random() * 10);
+        // console.log(data[ranNum].name);
+        newExercise = {
+            name: data[ranNum].name,
+            muscle: data[ranNum].muscle,
+            equipment: data[ranNum].equipment,
+            intructions: data[ranNum].instructions
+        };
+        exerciseGroup.push(newExercise);
     };
+    localStorage.setItem("exercises", JSON.stringify(exerciseGroup));
+    console.log(exerciseGroup);
 };
 
-// var muscleGroup = upper;
-// console.log(muscleGroup);
-// getExercises(muscleGroup);
 
+// calls getExercies depending on which button is clicked
 function generateCardEventHandler(event) {
     if (event.target.id === "upper") {
         getExercises(upper);
@@ -37,30 +43,6 @@ function generateCardEventHandler(event) {
     }
 };
 
+// event listener for modal with workout buttons
 generateCard.addEventListener("click", generateCardEventHandler);
 
-// var exercises = [
-//     {
-//         name: "tricep dips",
-//         muscle: "triceps",
-//         equipment: "dip_bars",
-//     },
-//     {
-//         name: "dumbbell benchpress",
-//         muscle: "chest",
-//         equipment: "dumbbells"
-//     }
-// ]
-
-// var armMuscle = "triceps"
-
-
-// async function exerciseApi() {
-//     const url = new URL("https://api.api-ninjas.com/v1/exercises");
-//     url.searchParams.set("muscle", armMuscle);
-//     const response = await fetch(url, { headers: { "X-API-Key": exerciseKey } });
-//     const data = await response.json();
-//     console.log(data);
-// };
-
-// exerciseApi();
