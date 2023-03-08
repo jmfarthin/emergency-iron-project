27652586d58a32435967f06276dd9ec4d8987c95
wyatt otto <wyattotto87@gmail.com>
2023-03-07 20:19:46 -0500

    added some new lines of JS to make card hidden

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,29 @@
 var button = document.getElementById('button')
-var modal = document.getElementById('Go-Modal')
+var HiddenCard = document.getElementById('Go-Card')
+var PromptCard = document.getElementById('card1')
+var Lower = document.getElementById('lower')
+var Upper = document.getElementById('upper')
+
+function loadIn(){
+    HiddenCard.classList.add('is-hidden')
+}
 
 button.onclick = function() {
-    modal.style.display = 'block';
-
+    
+    PromptCard.style.display = "none",
+    HiddenCard.classList.remove("is-hidden");
+    HiddenCard.classList.add("is-block");
 }
+loadIn() 
+
+Upper.addEventListener('click',remove)
+Lower.addEventListener('click',remove)
+
+function remove() {
+        HiddenCard.classList.remove("is-block");
+        HiddenCard.classList.add("is-hidden")
+    }
+
+   
+
+
